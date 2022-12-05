e889a33c4143c2d22adfdff5022a8be075d07288
EXT_API97 <EXT_API97@open-groupe.com>
2022-12-05 02:39:32 +0200

    Stable version

---
+++ components/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import type { NextComponentType } from 'next'
+
+const Button: NextComponentType = () => {
+  return (
+    <p className='text-blue-500 hover:bg-black'>Button</p>
+  )
+}
+
+export default Button
